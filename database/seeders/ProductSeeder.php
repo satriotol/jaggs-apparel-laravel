61c8fe8a494e28e63321ebe3cad45f92c52ccb1c
Satrio Jati <satriotol69@gmail.com>
2021-07-15 22:50:42 +0700

    product seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+
+
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+
+        for ($i = 0; $i < 4; $i++) {
+            $name = $faker->name;
+            Product::insert([
+                'name' => $name,
+                'slug' => Str::slug($name),
+                'weight' => random_int(100, 250),
+                'description' => $faker->text(),
+                'price' => random_int(100000, 200000),
+                'category_id' => random_int(1, 2),
+                'age_id' => random_int(1, 2),
+            ]);
+        }
+    }
+}

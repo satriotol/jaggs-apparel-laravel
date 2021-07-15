e455002522f6d46e2899a8ce1ecf72d3b60c27ad
Satrio Jati <satriotol69@gmail.com>
2021-07-15 22:50:50 +0700

    product seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ProductCategorySeeder::class,
-            AgeSeeder::class
+            AgeSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }

c628c85e2d6c43764a32a76845315b202bb799be
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2021-07-25 15:22:51 +0700

    response formatter

--- app/Http/Controllers/Api/ApiProductCategoryController.php
+++ app/Http/Controllers/Api/ApiProductCategoryController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\API\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\ProductCategory;
 use Illuminate\Http\Request;

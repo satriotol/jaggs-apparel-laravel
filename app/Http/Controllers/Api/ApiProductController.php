d211ea1eea1643de85f2059776d8caa6384337a9
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2021-07-25 15:57:25 +0700

    api product like product

--- app/Http/Controllers/Api/ApiProductController.php
+++ app/Http/Controllers/Api/ApiProductController.php
@@ -10,7 +10,6 @@ class ApiProductController extends Controller
 {
     public function index(Request $request)
     {
-        $slug = $request->input('slug');
         $category = $request->input('category_id');
         $product = Product::with(['category', 'galleries', 'product_size'])->whereHas('galleries')->whereHas('product_size', function ($q) {
             $q->where('qty', '>', 0);
@@ -19,17 +18,14 @@ class ApiProductController extends Controller
                 return ["category_name" => $value->category_name, "data" => $value];
             });
         });
-
-        if ($slug) {
-            $product = Product::with(['category', 'galleries', 'product_size'])->whereHas('galleries')->whereHas('product_size')->where('slug', $slug)->first();
-            if ($product) {
-                return ResponseFormatter::success($product, 'Data Product Berhasil Diambil');
-            } else {
-                return ResponseFormatter::error(null, 'Data produk tidak ada', 404);
-            };
-        }
         if ($category) {
-            $product = Product::with(['category', 'galleries', 'product_size'])->whereHas('galleries')->whereHas('product_size')->where('category_id', $category)->get();
+            $product = Product::with(['category', 'galleries', 'product_size'])->whereHas('galleries')->whereHas('product_size', function ($q) {
+                $q->where('qty', '>', 0);
+            })->where('category_id', $category)->get()->groupBy('category_name')->map(function ($group) {
+                return $group->map(function ($value) {
+                    return ["category_name" => $value->category_name, "data" => $value];
+                });
+            });
             if ($product) {
                 return ResponseFormatter::success($product, 'Data Product Berhasil Diambil');
             } else {

206908f2c559d84a21fd8aede072fabe3820d04d
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2021-08-07 02:50:42 +0700

    raja ongkir ini bos

---
+++ app/Http/Controllers/Api/RajaOngkirController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\RajaOngkirResource;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
+
+class RajaOngkirController extends Controller
+{
+    public function provinces()
+    {
+        $response = Http::withHeaders(['key' => '26494ebb2065e1306db10cf998995b5f'])->get('https://api.rajaongkir.com/starter/province');
+        return $response->json();
+    }
+    public function cities()
+    {
+        $response = Http::withHeaders(['key' => '26494ebb2065e1306db10cf998995b5f'])->get('https://api.rajaongkir.com/starter/city');
+        return $response->json();
+    }
+    public function cost()
+    {
+        $response = Http::withHeaders(['key' => '26494ebb2065e1306db10cf998995b5f'])->post('https://api.rajaongkir.com/starter/cost', [
+            "origin" => "501",
+            "destination" => "114",
+            "weight" => '1700',
+            "courier" => "jne"
+        ]);
+        return $response->json();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\ApiAboutController;
 use App\Http\Controllers\Api\ApiProductCategoryController;
 use App\Http\Controllers\Api\ApiProductController;
 use App\Http\Controllers\Api\CheckOutController;
+use App\Http\Controllers\Api\RajaOngkirController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +23,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('products', [ApiProductController::class, 'index']);
+Route::get('provinces', [RajaOngkirController::class, 'provinces']);
+Route::get('cities', [RajaOngkirController::class, 'cities']);
+Route::get('cost', [RajaOngkirController::class, 'cost']);
 Route::get('about', [ApiAboutController::class, 'index']);
 Route::get('products/{slug}', [ApiProductController::class, 'detail']);
 Route::get('productcategories', [ApiProductCategoryController::class, 'index']);

c9e8016ef202ff154cd7544e45e464ffda3f04ab
SATRIO <satriotol69@gmail.com>
2021-12-12 10:31:49 +0700

    fixing

--- app/Http/Controllers/Api/ApiProductCategoryController.php
+++ app/Http/Controllers/Api/ApiProductCategoryController.php
@@ -10,7 +10,11 @@ class ApiProductCategoryController extends Controller
 {
     public function index()
     {
-        $product_category = ProductCategory::whereHas('products')->get();
+        $product_category = ProductCategory::whereHas('products', function ($q) {
+            $q->where('is_sale', 0)->whereHas('galleries')->whereHas('product_size', function ($sq) {
+                $sq->where('qty', '>', '0');
+            });
+        })->get();
         if ($product_category) {
             return ResponseFormatter::success($product_category, 'Data Product Category Berhasil Diambil');
         } else {

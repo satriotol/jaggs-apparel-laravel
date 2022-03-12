cb4ae33b4939aaba85d0f2e4ad93135914f16242
SATRIO <satriotol69@gmail.com>
2022-03-12 10:42:01 +0700

    fixing

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -1,63 +1,67 @@
 @extends('layouts.main')
 @section('content')
-@include('partials.success')
-@include('partials.error')
-<div class="row">
-    <div class="col-md-6">
-        <div class="card card-default">
-            <div class="card-header card-header-border-bottom">
-                <h2>Product Detail</h2>
-            </div>
+    @include('partials.success')
+    @include('partials.error')
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card card-default">
+                <div class="card-header card-header-border-bottom">
+                    <h2>Product Detail</h2>
+                </div>
 
-            <div class="card-body">
+                <div class="card-body">
 
-                <div class="form-group">
-                    <label for="exampleFormControlInput1">Product Name</label>
-                    <input type="text" disabled class="form-control" name="name"
-                        value="{{isset($product) ? $product->name : ''}}" id="exampleFormControlInput1"
-                        placeholder="Enter Product Name">
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlInput1">Price</label>
-                    <input type="number" disabled class="form-control" name="price"
-                        value="{{isset($product) ? $product->price : ''}}" id=" exampleFormControlInput1"
-                        placeholder="Enter Product Price">
-                </div>
-                <div class="form-group d-none">
-                    <label for="exampleFormControlInput1">New Price</label>
-                    <input type="number" disabled class="form-control" name="new_price"
-                        value="{{isset($product) ? $product->new_price : ''}}" id=" exampleFormControlInput1"
-                        placeholder="Enter Product Price">
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Description</label>
-                    <textarea class="form-control" disabled name="description" id="exampleFormControlTextarea1"
-                        rows="3">{{isset($product) ? $product->description : ''}}</textarea>
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Weight</label>
-                    <input type="number" class="form-control" name="weight" disabled
-                        value="{{isset($product) ? $product->weight : ''}}">
-                </div>
-                <div class="form-group d-none">
-                    <label>Is Sale ?</label>
-                    <select name="is_sale" class="form-control" required disabled>
-                        <option value="0" @if (isset($product)) @if (0===$product->is_sale)
-                            selected @endif @endif>No</option>
-                        <option value="1" @if (isset($product)) @if (1===$product->is_sale)
-                            selected @endif @endif>Yes</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlSelect12">Category</label>
-                    <select class="form-control" id="exampleFormControlSelect12" disabled name="category_id">
-                        <option>Select Category</option>
-                        @foreach ($categories as $category)
-                        <option value="{{$category->id}}" @if (isset($product)) @if ($category->id ===
-                            $product->category_id)
-                            selected @endif @endif>
-                            {{$category->name}}
-                        </option>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Product Name</label>
+                        <input type="text" disabled class="form-control" name="name"
+                            value="{{ isset($product) ? $product->name : '' }}" id="exampleFormControlInput1"
+                            placeholder="Enter Product Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Price</label>
+                        <input type="number" disabled class="form-control" name="price"
+                            value="{{ isset($product) ? $product->price : '' }}" id=" exampleFormControlInput1"
+                            placeholder="Enter Product Price">
+                    </div>
+                    <div class="form-group d-none">
+                        <label for="exampleFormControlInput1">New Price</label>
+                        <input type="number" disabled class="form-control" name="new_price"
+                            value="{{ isset($product) ? $product->new_price : '' }}" id=" exampleFormControlInput1"
+                            placeholder="Enter Product Price">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Description</label>
+                        <textarea class="form-control" disabled name="description" id="exampleFormControlTextarea1"
+                            rows="3">{{ isset($product) ? $product->description : '' }}</textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Weight</label>
+                        <input type="number" class="form-control" name="weight" disabled
+                            value="{{ isset($product) ? $product->weight : '' }}">
+                    </div>
+                    <div class="form-group d-none">
+                        <label>Is Sale ?</label>
+                        <select name="is_sale" class="form-control" required disabled>
+                            <option value="0"
+                                @if (isset($product)) @if (0 === $product->is_sale)
+                            selected @endif
+                                @endif>No</option>
+                            <option value="1"
+                                @if (isset($product)) @if (1 === $product->is_sale)
+                            selected @endif
+                                @endif>Yes</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlSelect12">Category</label>
+                        <select class="form-control" required disabled name="category_id">
+                            <option value="">Select Category</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}"
+                                    @isset($product) @if ($category->name === $product->category->name) selected @endif
+                                @endisset>
+                                {{ $category->name }}
+                            </option>
                         @endforeach
                     </select>
                 </div>
@@ -68,7 +72,7 @@
         <div class="card card-table-border-none" id="recent-orders">
             <div class="card-header justify-content-between">
                 <h2>Product Quantity</h2>
-                <a class="btn btn-primary" href="{{route('quantity.create',$product->id)}}">
+                <a class="btn btn-primary" href="{{ route('quantity.create', $product->id) }}">
                     Create
                 </a>
             </div>
@@ -82,15 +86,16 @@
                     </thead>
                     <tbody>
                         @foreach ($sizes as $size)
-                        <tr>
-                            <td>{{$size->name}}</td>
-                            <td> @foreach ($size->product_size as $ps)
-                                @if ($ps->product_id == $product->id)
-                                {{$ps->qty}}
-                                @endif
-                                @endforeach
-                            </td>
-                        </tr>
+                            <tr>
+                                <td>{{ $size->name }}</td>
+                                <td>
+                                    @foreach ($size->product_size as $ps)
+                                        @if ($ps->product_id == $product->id)
+                                            {{ $ps->qty }}
+                                        @endif
+                                    @endforeach
+                                </td>
+                            </tr>
                         @endforeach
 
                     </tbody>
@@ -100,7 +105,7 @@
         <div class="card card-table-border-none" id="recent-orders">
             <div class="card-header justify-content-between">
                 <h2>Product Gallery</h2>
-                <a class="btn btn-primary" href="{{route('gallery.create',$product->id)}}">
+                <a class="btn btn-primary" href="{{ route('gallery.create', $product->id) }}">
                     Create
                 </a>
             </div>
@@ -114,17 +119,18 @@
                     </thead>
                     <tbody>
                         @foreach ($galleries as $gallery)
-                        <tr>
-                            <td><img src="{{$gallery->photo}}" height="100px" alt=""></td>
-                            <td>
-                                <form action="{{route('gallery.destroy',$gallery->id)}}" class="d-inline" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="badge badge-danger" onclick="return confirm('Are you sure?')"
-                                        type="submit">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
+                            <tr>
+                                <td><img src="{{ $gallery->photo }}" height="100px" alt=""></td>
+                                <td>
+                                    <form action="{{ route('gallery.destroy', $gallery->id) }}"
+                                        class="d-inline" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="badge badge-danger" onclick="return confirm('Are you sure?')"
+                                            type="submit">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
                         @endforeach
 
                     </tbody>
